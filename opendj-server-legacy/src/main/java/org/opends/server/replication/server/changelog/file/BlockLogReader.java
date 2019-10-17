02b1749dfffb20e16d5e3ce5c0b09dbb43179db0
Tsujiguchi Takaya <tsujiguchi@osstech.co.jp>
2019-10-17 20:33:23 +0900

    Issue openam#81 Unable to change settings due to changelogDb issue of embedded DJ (#2)
    * OPENDJ-2969 Improve error message
    
    Included the stack trace of the original exception in the error message to help with diagnosis
    
    * OPENDJ-2969 OPENDJ-2978 OPENDJ-2991 Fix BlockLogReader#getNewestRecord() method
    
    BlockLogReader#getNewestRecord() was failing with EOF if log file size was a
    multiple of 256 (internal block size)
    
    * OPENDJ-2969 Fix BlockLogReader#checkLogIsValid() method
    
    closed #81

--- opendj-server-legacy/src/main/java/org/opends/server/replication/server/changelog/file/BlockLogReader.java
+++ opendj-server-legacy/src/main/java/org/opends/server/replication/server/changelog/file/BlockLogReader.java
@@ -20,8 +20,7 @@
  *
  * CDDL HEADER END
  *
- *
- *      Copyright 2014-2015 ForgeRock AS.
+ * Copyright 2014-2016 ForgeRock AS.
  */
 package org.opends.server.replication.server.changelog.file;
 
@@ -39,6 +38,7 @@ import org.forgerock.opendj.ldap.ByteString;
 import org.forgerock.opendj.ldap.ByteStringBuilder;
 import org.forgerock.util.Pair;
 import org.forgerock.util.Reject;
+import org.forgerock.util.annotations.VisibleForTesting;
 import org.opends.server.replication.server.changelog.api.ChangelogException;
 import org.opends.server.replication.server.changelog.api.DBCursor.KeyMatchingStrategy;
 import org.opends.server.replication.server.changelog.api.DBCursor.PositionStrategy;
@@ -540,10 +540,20 @@ class BlockLogReader<K extends Comparable<K>, V> implements Closeable
    *          The position of reader on file.
    * @return the file position of the block start.
    */
+  @VisibleForTesting
   long getClosestBlockStartBeforeOrAtPosition(final long filePosition)
   {
     final int dist = getDistanceToNextBlockStart(filePosition, blockSize);
     return dist == 0 ? filePosition : filePosition + dist - blockSize;
+  }
+
+  @VisibleForTesting
+  long getClosestBlockStartToEndOfFile() throws ChangelogException
+  {
+    final long fileLength = getFileLength();
+    long lastBlockStart = getClosestBlockStartBeforeOrAtPosition(fileLength);
+    // block start is invalid if equal to file length
+    return lastBlockStart < fileLength ? lastBlockStart :  getClosestBlockStartBeforeOrAtPosition(fileLength-1);
   }
 
   /**
@@ -599,8 +609,7 @@ class BlockLogReader<K extends Comparable<K>, V> implements Closeable
  {
    try
    {
-     final long fileSize = getFileLength();
-     final long lastBlockStart = getClosestBlockStartBeforeOrAtPosition(fileSize);
+     final long lastBlockStart = getClosestBlockStartToEndOfFile();
      positionToRecordFromBlockStart(lastBlockStart);
 
      long lastValidPosition = lastBlockStart;
@@ -609,7 +618,7 @@ class BlockLogReader<K extends Comparable<K>, V> implements Closeable
        lastValidPosition = reader.getFilePointer();
      }
 
-     final boolean isFileValid = lastValidPosition == fileSize;
+     final boolean isFileValid = lastValidPosition == getFileLength();
      return isFileValid ? -1 : lastValidPosition;
    }
    catch (Exception e)
@@ -623,7 +632,7 @@ class BlockLogReader<K extends Comparable<K>, V> implements Closeable
 Record<K, V> getNewestRecord() throws ChangelogException
  {
    try {
-     final long lastBlockStart = getClosestBlockStartBeforeOrAtPosition(getFileLength());
+     long lastBlockStart = getClosestBlockStartToEndOfFile();
      positionToRecordFromBlockStart(lastBlockStart);
      ByteString candidate = readNextRecord();
      ByteString record = candidate;
